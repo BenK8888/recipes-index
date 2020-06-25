8b0c6ee54a7bea54b0599ffb3a38ebf1a757118c
BenK8888 <kolberben@gmail.com>
2020-06-25 11:46:20 +0300

    initialize menu component and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import RecipesGrid from './RecipesGrid';
+import Nav from './Nav';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 
 const App = () => {
   return (
-    <RecipesGrid />
+    <Router>
+      <div>
+        <Nav />
+        <Route path="/" exact component={RecipesGrid}/>
+      </div>
+    </Router>
   );
 };
 
