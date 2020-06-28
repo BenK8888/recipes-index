275a69a39ffe8009b737d7833480b4e231cc6af7
BenK8888 <kolberben@gmail.com>
2020-06-28 16:16:03 +0300

    make design consistent by moving constant design to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import titlePhoto from '../pics/title.png';
 import RecipesGrid from './RecipesGrid';
 import RecipePage from './RecipePage';
 import Nav from './Nav';
@@ -9,8 +10,10 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 const App = () => {
   return (
     <Router>
-      <div>
-        <Nav />
+      <Nav />
+      <div className="App">
+        <div className="titlePictureDiv"><img className="titlePicture" src={titlePhoto} alt=""/></div>
+        <div className="subTitle">What would you like to cook today?</div>
         <Route path="/" exact component={RecipesGrid}/>
         <Route path="/:label" component={RecipePage}/>
       </div>
